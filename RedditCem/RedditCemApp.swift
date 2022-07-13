bdc5f2b20f341a572b52f9457d27b9710f53673e
Cem Uğurlu <cemugurlu5@gmail.com>
2022-07-13 15:12:16 +0300

    Initial Commit

---
+++ RedditCem/RedditCemApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditCemApp.swift
+//  RedditCem
+//
+//  Created by Cem Uğurlu on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct RedditCemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
